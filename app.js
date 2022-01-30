8047ed34b9419af3fc34b39498899400a7c453c3
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-01-30 15:27:51 -0500

    Navbar finished

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 
-// const mainBtns = document.querySelectorAll('.main-btn')
-//
-// mainBtns.forEach(btn => {
-//   let ripple;
-//   mainBtns.addEventListener('mouseenter',(e) => {
-//     const left = e.clientX - e.target.getBoundingClientRect().left;
-//     const top = e.clientY - e.target.getBoundingClientRect().top;
-//
-//     ripple = document.createElement('div')
-//     ripple.classList.add('ripple')
-//     ripple.style.left = `${left}px`
-//     ripple.style.top =  `${top}px`
-//     mainBtns.prepend(ripple)
-//   })
-//   mainBtns.addEventListener('mouseleave',() => {
-//     mainBtns.removeChild(ripple)
-//   })
-// })
-
-const aboutMeText = document.querySelector('.about-me-text')
-const aboutMeTextContent = 'I am a software developer and I like creating things'
-Array.from(aboutMeTextContent).forEach(char => {
-  const span = document.createElement('span')
-  span.textContent = char
-  aboutMeText.append(span)
-
-  span.addEventListener('mouseenter', e => {
-    e.target.style.animation = "aboutMeTextAnim 10s infinite"
-  })
+// console.log("linked");
+$(window).scroll(()  => {
+  if ($(this).scrollTop() >= 5){
+    $('.navbar').addClass('sticky')
+  } else if ($(this).scrollTop() < 5) {
+    $('.navbar').removeClass('sticky')
+  }
 })
+
+$('.menu-btn').click(() => {
+  $('.navbar .menu').toggleClass('active');
+
+})
+
+// (() => {
+//   $(window).on('scroll',sticky)
+// })
