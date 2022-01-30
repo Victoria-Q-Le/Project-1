cf3e012c2f60d1ac040d7feedc3a8925d57e4e50
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-01-29 23:13:05 -0500

    Remake 2 - Background, common styles

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-console.log("linked");
+
+// const mainBtns = document.querySelectorAll('.main-btn')
+//
+// mainBtns.forEach(btn => {
+//   let ripple;
+//   mainBtns.addEventListener('mouseenter',(e) => {
+//     const left = e.clientX - e.target.getBoundingClientRect().left;
+//     const top = e.clientY - e.target.getBoundingClientRect().top;
+//
+//     ripple = document.createElement('div')
+//     ripple.classList.add('ripple')
+//     ripple.style.left = `${left}px`
+//     ripple.style.top =  `${top}px`
+//     mainBtns.prepend(ripple)
+//   })
+//   mainBtns.addEventListener('mouseleave',() => {
+//     mainBtns.removeChild(ripple)
+//   })
+// })
 
 const aboutMeText = document.querySelector('.about-me-text')
 const aboutMeTextContent = 'I am a software developer and I like creating things'
@@ -6,4 +24,8 @@ Array.from(aboutMeTextContent).forEach(char => {
   const span = document.createElement('span')
   span.textContent = char
   aboutMeText.append(span)
+
+  span.addEventListener('mouseenter', e => {
+    e.target.style.animation = "aboutMeTextAnim 10s infinite"
+  })
 })
