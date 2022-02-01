6a395f1972d048ca970fa0afcfbeaa2e4ebff051
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-01-31 19:44:58 -0500

    Photo caption

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ $('.menu-btn').click(() => {
   $('.navbar .menu').toggleClass('active');
 })
 
-
+// Carousel Lesson in class
 let currentImgIndex = 0
 const numberOfImages = $('.carousel-images').children().length - 1
 $('.next').on('click', () => {
@@ -25,25 +25,24 @@ $('.next').on('click', () => {
   $('.carousel-images').children().eq(currentImgIndex).css('display','block')
 })
 
+$('.previous').on('click', () => {
+  $('.carousel-images').children().eq(currentImgIndex).css('display','none')
+  if (currentImgIndex > 0){
+    currentImgIndex--
+  } else {
+    currentImgIndex = numberOfImages
+  }
+  $('.carousel-images').children().eq(currentImgIndex).css('display','block')
+})
 
-//  let currentImgIndex = 0;
-// const numberOfImages = $('carousel-images').children().length - 1
-// $('.next').on('click', () => {
-//   $('.carousel-images').children().eq(currentImgIndex).css('display','none')
-//   if ( currentImgIndex < numberOfImages) {
-//     currentImgIndex++
+// let currentImgCaption = 0
+// const numberOfCaption = $('.caption').children().length - 1
+// $('.next').hover(() => {
+//   $('.caption').children().eq(currentImgCaption).css('display','none')
+//   if (currentImgCaption < numberOfCaption){
+//     currentImgCaption++
 //   } else {
-//     currentImgIndex = 0
+//     currentImgCaption = 0
 //   }
-//   $('.carousel-images').children().eq(currentImgIndex).css('display','block')
-// })
-//
-// $('.previous').on('click', () => {
-//   $('.carousel-images').children()eq(currentImgIndex).css('display','block')
-//   if (currentImgIndex > 0){
-//     currentImgIndex--
-//   } else {
-//     currentImgIndex = numberOfImages
-//   }
-//   $('.carousel-images').children().eq(currentImgIndex).css('display','block')
+//   $('.caption').children().eq(currentImgCaption).css('display',)
 // })
